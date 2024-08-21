05a561d7f6a8752a26cab437132f314772e13631
TatTatTaRa <kjm17092@gmail.com>
2024-08-21 11:27:45 +0900

    테스트

---
+++ .next/static/hYAG3geu9yhCyQ-YPpMHi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/521-238384dc4ce265c4.js","static/chunks/pages/index-b25fab331d714709.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
