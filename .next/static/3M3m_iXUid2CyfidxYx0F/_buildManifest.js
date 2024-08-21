2a331bd6339281c7d4369115210b27d18ab8911d
TatTatTaRa <kjm17092@gmail.com>
2024-08-21 10:40:33 +0900

    리액트 테스트

---
+++ .next/static/3M3m_iXUid2CyfidxYx0F/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/571-b5fef310c8ee54f3.js","static/chunks/pages/index-cc1aeed23b11c14e.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
